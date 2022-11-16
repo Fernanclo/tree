9373e01da704f0f0f2c5173e025b10ce7f99ceba
Fernanclo <117941935+Fernanclo@users.noreply.github.com>
2022-11-16 14:00:48 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,93 @@
+basic.showLeds(`
+    . # # # .
+    # # # # #
+    # # # # #
+    . . # . .
+    . . # . .
+    `)
+basic.pause(1500)
+basic.showString(" TREE")
+basic.pause(500)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # # # # #
+    . # # # .
+    . # # # .
+    `)
+basic.pause(1500)
+basic.showString("House")
+basic.pause(500)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+basic.pause(1500)
+basic.showString("Smiley Face")
+basic.pause(500)
+basic.showLeds(`
+    . . # # .
+    . . # . .
+    . . # . .
+    # # # # #
+    . # # # .
+    `)
+basic.pause(1500)
+basic.showString("Boat")
+basic.pause(500)
+basic.showLeds(`
+    . # # # .
+    . # # # .
+    . # # # .
+    # # # # #
+    . # # # .
+    `)
+basic.pause(1500)
+basic.showString("Hat")
+basic.pause(500)
+basic.showLeds(`
+    # # # . .
+    # # # # #
+    # # # . #
+    # # # # #
+    # # # . .
+    `)
+basic.pause(1500)
+basic.showString("Mug")
+basic.pause(500)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . # # # .
+    # # # # #
+    `)
+basic.pause(1500)
+basic.showString("Pyramid")
+basic.pause(500)
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . # . # .
+    # . . . #
+    # . . . #
+    `)
+basic.pause(1500)
+basic.showString("HeadPhones")
+basic.pause(500)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # # # # #
+    . . # . .
+    . . # . .
+    `)
+basic.pause(1500)
+basic.showString("Arrow")
+basic.pause(500)
+basic.forever(function () {
+	
+})
